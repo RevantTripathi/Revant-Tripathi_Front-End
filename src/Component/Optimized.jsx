3bcafc26f9794c19c708cd505a654208e27ddf1f
Revant Tripathi <revanttripathi8@gmail.com>
2023-04-23 00:59:45 +0530

    Final Optimized

--- src/Component/Optimized.jsx
+++ src/Component/Optimized.jsx
@@ -1,32 +1,34 @@
 import React, { useState, useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
+import SingleListItem from './OptimizedSingleListComponent';
 
 
 //Single List Item
-const WrappedSingleListItem = ({
-    index,
-    isSelected,
-    onClickHandler,
-    text,
-}) => {
-    return (
-        <li
-            style={{ backgroundColor: isSelected ? 'green' : 'red' }}
-            onClick={() => onClickHandler(index)}
-        >
-            {text}
-        </li>
-    );
-};
 
-WrappedSingleListItem.propTypes = {
-    index: PropTypes.number,
-    isSelected: PropTypes.bool,
-    onClickHandler: PropTypes.func.isRequired,
-    text: PropTypes.string.isRequired,
-};
+// const WrappedSingleListItem = ({
+//     index,
+//     isSelected,
+//     onClickHandler,
+//     text,
+// }) => {
+//     return (
+//         <li
+//             style={{ backgroundColor: isSelected ? 'green' : 'red' }}
+//             onClick={() => onClickHandler(index)}
+//         >
+//             {text}
+//         </li>
+//     );
+// };
 
-const SingleListItem = memo(WrappedSingleListItem);
+// WrappedSingleListItem.propTypes = {
+//     index: PropTypes.number,
+//     isSelected: PropTypes.bool,
+//     onClickHandler: PropTypes.func.isRequired,
+//     text: PropTypes.string.isRequired,
+// };
+
+// const SingleListItem = memo(WrappedSingleListItem);
 
 // List Component
 const WrappedListComponent = ({
